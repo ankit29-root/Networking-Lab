5432c996b793e27f3cb6b30bec1701d442f183df
ankit_29 <53056357+ankit29-root@users.noreply.github.com>
2023-04-19 18:23:53 +0530

    Update Echo Server.java

--- Echo Server.java
+++ Echo Server.java
@@ -20,8 +20,7 @@ try
 while (true)
 {
 Socket client = server.accept();
-BufferedReader r = new BufferedReader(new
-InputStreamReader(client.getInputStream()));
+BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
 PrintWriter w = new PrintWriter(client.getOutputStream(),true);
 w.println("Welcome to the Java EchoServer. Type 'bye'to close.");
 String line;
