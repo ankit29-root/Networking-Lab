b7310c75b19ab0a77ebabf1e32345041eef396eb
ankit_29 <53056357+ankit29-root@users.noreply.github.com>
2023-05-18 22:47:12 +0530

    Update Stopwait Receiver.java

--- Stopwait Receiver.java
+++ Stopwait Receiver.java
@@ -1,27 +1,20 @@
 import java.io.*;
 import java.net.*;
-class stopwaitreceiver
-{
-public static void main(String args[]) throws Exception
-{
+class stopwaitreceiver{
+public static void main(String args[]) throws Exception{
 stopwaitreceiver swr = new stopwaitreceiver();
 swr.run();
 }
-public void run() throws Exception
-{
+public void run() throws Exception{
 String temp="any message" , str="exit";
 ServerSocket myss=new ServerSocket(9999);
 Socket ss_accept=myss.accept();
-BufferedReader ss_bf=new BufferedReader(new
-InputStreamReader(ss_accept.getInputStream()));
-PrintStream myps=new
-PrintStream(ss_accept.getOutputStream());
-while(temp.compareTo(str)!=0)
-{
+BufferedReader ss_bf=new BufferedReader(new InputStreamReader(ss_accept.getInputStream()));
+PrintStream myps=new PrintStream(ss_accept.getOutputStream());
+while(temp.compareTo(str)!=0){
 Thread.sleep(1000);
 temp=ss_bf.readLine();
-if(temp.compareTo(str)==0)
-{
+if(temp.compareTo(str)==0){
 break;
 }
 Thread.sleep(500);
