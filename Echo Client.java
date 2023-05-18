75edc09e7afe23b60c2a5b18c7a28d21a1082717
ankit_29 <53056357+ankit29-root@users.noreply.github.com>
2023-05-18 22:43:46 +0530

    Update Echo Client.java

--- Echo Client.java
+++ Echo Client.java
@@ -1,19 +1,15 @@
 import java.io.*;
 import java.net.*;
-public class EchoClient
-{
-public static void main(String[] args)
-{
-try
-{
+public class EchoClient{
+public static void main(String[] args){
+try{
 Socket s = new Socket("127.0.0.1", 9999);
 BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
 PrintWriter w = new
 PrintWriter(s.getOutputStream(), true);
 BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
 String line;
-do
-{
+do{
 line = r.readLine();
 if ( line != null )
 System.out.println(line);
@@ -22,8 +18,7 @@ w.println(line);
 }
 while ( !line.trim().equalsIgnoreCase("bye") );
 }
-catch (Exception err)
-{
+catch (Exception err){
 System.err.println(err);
 }
 }
