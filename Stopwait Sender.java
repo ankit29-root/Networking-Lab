d590a89164708225a4e88b0983bd56e516e4072b
ankit_29 <53056357+ankit29-root@users.noreply.github.com>
2023-05-18 22:48:05 +0530

    Update Stopwait Sender.java

--- Stopwait Sender.java
+++ Stopwait Sender.java
@@ -1,41 +1,31 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-class stopwaitsender
-{
-public static void main(String args[]) throws Exception
-{
+class stopwaitsender{
+public static void main(String args[]) throws Exception{
 stopwaitsender sws=new stopwaitsender();
 sws.run();
 }
-public void run() throws Exception
-{
+public void run() throws Exception{
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter no of frames to be sent");
 int n=sc.nextInt();
 Socket myskt=new Socket("localhost" , 9999);
-PrintStream myps=new
-PrintStream(myskt.getOutputStream());
-for(int i=0;i<=n;i++)
-{
-if(i==n)
-{
+PrintStream myps=new PrintStream(myskt.getOutputStream());
+for(int i=0;i<=n;i++){
+if(i==n){
 myps.println("exit");
 break;
 }
 System.out.println("Frame no "+i+" is sent");
 myps.println(i);
-BufferedReader bf=new
-BufferedReader(new
-InputStreamReader(myskt.getInputStream()));
+BufferedReader bf=new BufferedReader(new InputStreamReader(myskt.getInputStream()));
 String ack=bf.readLine();
-if(ack!=null)
-{
+if(ack!=null){
 System.out.println("Acknowledgement was Received from receiver");
 Thread.sleep(4000);
 }
-else
-{
+else{
 myps.println(i);
 }
 }
