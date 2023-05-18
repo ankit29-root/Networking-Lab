7ec7484de23475c74b4214584b5dff6187a97431
ankit_29 <53056357+ankit29-root@users.noreply.github.com>
2023-05-18 22:43:01 +0530

    Update Date Server.java

--- Date Server.java
+++ Date Server.java
@@ -1,20 +1,16 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-class dateserver
-{
-public static void main(String args[])
-{
+class dateserver{
+public static void main(String args[]){
 ServerSocket ss;
 Socket s;
 PrintStream ps;
 DataInputStream dis;
 String inet;
-try
-{
+try{
 ss=new ServerSocket(8020);
-while(true)
-{
+while(true){
 s=ss.accept();
 ps=new PrintStream(s.getOutputStream());
 Date d=new Date();
@@ -25,7 +21,8 @@ System.out.println("IP Address of the client is : "+inet);
 ps.close();
 }
 }
-catch(IOException e)
-{
+catch(IOException e){
 System.out.println("The exception is: "+e);
-} } }
+} 
+} 
+}
